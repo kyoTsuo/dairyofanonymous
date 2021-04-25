c6caa8e6292a103f84a0aaadd09577eea49ca7a4
mnmnkmmn <79610912+mnmnkmmn@users.noreply.github.com>
2021-04-25 21:10:46 +0900

    編集機能追加
    編集：更新・削除アクションの追加

--- app/controllers/dairies_controller.rb
+++ app/controllers/dairies_controller.rb
@@ -12,6 +12,19 @@ class DairiesController < ApplicationController
   end
 
   def edit
+    @dairy = Dairy.find(params[:id])
+  end
+
+  def update
+    dairy = Dairy.find(params[:id])
+    dairy.update(dairy_params)
+    redirect_to dairies_url, notice: "日記#{dairy.title}を更新しました。"
+  end
+
+  def destroy
+    dairy = Dairy.find(params[:id])
+    dairy.destroy
+    redirect_to dairies_url, notice: "日記#{dairy.title}を削除しました。"
   end
 
   def create
