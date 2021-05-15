330430d3a0afc56f933930eb8c9cda5db33b6cca
mnmnkmmn <79610912+mnmnkmmn@users.noreply.github.com>
2021-05-15 21:44:43 +0900

    マイグレーション適用後

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2021_05_08_120809) do
+ActiveRecord::Schema.define(version: 2021_05_09_065147) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +26,15 @@ ActiveRecord::Schema.define(version: 2021_05_08_120809) do
     t.index ["user_id"], name: "index_dairies_on_user_id"
   end
 
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "dairy_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["dairy_id"], name: "index_favorites_on_dairy_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "password_digest", null: false
@@ -33,4 +43,6 @@ ActiveRecord::Schema.define(version: 2021_05_08_120809) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "favorites", "dairies"
+  add_foreign_key "favorites", "users"
 end
