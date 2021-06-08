34969a892fc341dd3658fcc57997cf64ee1d464a
mnmnkmmn <79610912+mnmnkmmn@users.noreply.github.com>
2021-06-09 07:33:38 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
     has_secure_password
 
     validates :email, presence: true, uniqueness: true
+    validates :password, length: { minimum: 8 }, confirmation: true
 
     has_many :dairies, dependent: :destroy
     has_many :favorites, dependent: :destroy
