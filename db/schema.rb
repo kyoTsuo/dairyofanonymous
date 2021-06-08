5ce0a5b928cd3dd6d56e6934f12ae896a26cb00e
mnmnkmmn <79610912+mnmnkmmn@users.noreply.github.com>
2021-06-09 07:29:52 +0900

    draftテーブルの削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_01_113631) do
+ActiveRecord::Schema.define(version: 2021_06_06_121715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,16 +24,6 @@ ActiveRecord::Schema.define(version: 2021_06_01_113631) do
     t.bigint "user_id", null: false
     t.boolean "drafted", default: false, null: false
     t.index ["user_id"], name: "index_dairies_on_user_id"
-  end
-
-  create_table "drafts", force: :cascade do |t|
-    t.string "title"
-    t.text "content", null: false
-    t.boolean "published", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_drafts_on_user_id"
   end
 
   create_table "favorites", force: :cascade do |t|
@@ -53,7 +43,6 @@ ActiveRecord::Schema.define(version: 2021_06_01_113631) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
-  add_foreign_key "drafts", "users"
   add_foreign_key "favorites", "dairies"
   add_foreign_key "favorites", "users"
 end
